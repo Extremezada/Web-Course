c0931da8c13094ddd87557e15214fcd06a0b82f2
Extremezada <rafa.dalmagro@hotmail.com>
2023-09-04 10:36:35 -0300

    Iniciando módulo sobre Webpack, tive alguns problemas para rodar os códigos, fiz um downgrade da versão 18 do node para a versão 16.

---
+++ webpack/src/pessoa.js
@@ -0,0 +1,8 @@
+
+
+export default class Pessoa{
+    cumprimentar(){
+        return 'Bom dia!'
+    }
+}
+
---
+++ webpack/src/principal.js
@@ -0,0 +1,5 @@
+import Pessoa from "./pessoa";
+
+const atentdente = new Pessoa
+
+console.log(atentdente.cumprimentar())
