2326e940a10e27a262ee325c0ba9fcfd08f34681
Extremezada <rafa.dalmagro@hotmail.com>
2023-09-20 11:04:29 -0300

    Adicionando arquivos estilizados com Css e o inicio do processo de cadastro de Usuários!

--- react/crud/frontend/src/components/templates/Nav.jsx
+++ react/crud/frontend/src/components/templates/Nav.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Nav.css"
+import NavItem from "./NavItem";
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Refatorar */}
+            <NavItem icon="home" text="Início" />
+            <NavItem icon="user" text="Usuário" />
+        </nav>
     </aside>
--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import "font-awesome/css/font-awesome.min.css"
 
-import Main from "../components/templates/Main"
+import { BrowserRouter, Routes } from "react-router-dom";
+
 import Footer from "../components/templates/Footer"
 import Logo from "../components/templates/Logo"
 import Nav from "../components/templates/Nav"
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Main />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
