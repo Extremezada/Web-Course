7dd4123b48d229b968f0c9021f080501cfa3b873
Extremezada <rafa.dalmagro@hotmail.com>
2023-09-23 14:30:17 -0300

    Adicionando arquivos sobre interações com API.

--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -9,12 +9,15 @@ export default function Incicio(){
             justifyContent: "center",
             height: "100vh"
         }}>
-            <Navegador cor="crimson" nomePg="Estiloso" destino="/estiloso" />
+            <Navegador cor="blue" nomePg="Estiloso" destino="/estiloso" />
             <Navegador cor="purple" nomePg="Exemplo" destino="/exemplo" />
             <Navegador nomePg="JSX" destino="/jsx" />
             <Navegador nomePg="Navegação #01" destino="/navegacao" 
             cor="green"/>
             <Navegador nomePg="Navegação #02" destino="/client/sp-22/123" cor="red" />
+            <Navegador nomePg="Componente de estado" destino="/estado" cor="yellow" />
+            <Navegador nomePg="API" destino="/integracao" cor="crimson" />
+
         </div>
     )
 }
