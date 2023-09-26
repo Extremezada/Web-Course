fcdd23bc186fd5bc49e219c67db221d1f39be214
Extremezada <rafa.dalmagro@hotmail.com>
2023-09-26 11:17:03 -0300

    Adicionando arquivos de tabela e botões, adicionado layout da tabela e funcões para os botões de edição e exclusão.

--- projeto-cadastro/next-crud/src/pages/index.tsx
+++ projeto-cadastro/next-crud/src/pages/index.tsx
@@ -1,12 +1,36 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
-  return (
-    <div className=''>
-      <span>Texto</span>
-    </div>
-  )
+
+    const clientes = [
+        new Cliente('Ana', 23, '1'),
+        new Cliente('Pedro', 19, '2'),
+        new Cliente('Maria', 39, '3'),
+        new Cliente('Felipe', 47, '4'),
+    ]
+
+    function clienteSelecionado(cliente: Cliente){
+        console.log(cliente.nome)
+    }
+    function clienteExcluido(cliente: Cliente){
+        console.log(`Excluindo...${cliente.nome}`)
+    }
+    
+    return (
+        <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+            <Layout titulo="Cadastro Simples">
+                <Tabela 
+                    clientes={clientes} 
+                    clienteExcluido={clienteExcluido}
+                    clienteSelecionado={clienteSelecionado} 
+                />
+            </Layout>
+        </div>
+    )
 }
